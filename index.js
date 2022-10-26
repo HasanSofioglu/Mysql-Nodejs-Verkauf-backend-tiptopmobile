455b48a4bdfbf881f0b32130bf471610fb00b1f1
Hasan Sofioğlu <h.sofioglu21@gmail.com>
2022-10-26 16:15:25 +0200

    test

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const { markAsUntransferable } = require('worker_threads');
 
 
 const db = mysql.createPool({
-    host:"159.223.22.74",
-    user: "root",
-    password:"19682121",
+    host:"%",
+    user: "admin",
+    password:"admin",
     database:"VERKAUFDataBase", 
 });
 const fileUpload = require('express-fileupload');
